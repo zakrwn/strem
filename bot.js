a48c43b486f06363984f920fe92efed6cfb27792
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-11-14 16:20:50 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,16 @@
-const Discord = require('discord.js'),
-    bot = new Discord.Client({sisableEveryone: true})
-    console.log("muuuuute,");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-
-
-
- const devs = ['295909228328255488'];
-const adminprefix = "!";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**  ${argresult} ply .. :white_check_mark:**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**  ${argresult} wt .. :white_check_mark:**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**  ${argresult} ls .. :white_check_mark:**`)
-  } else 
+var prefix = "zz";
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
       message.channel.sendMessage(`**  ${argresult} st .. :white_check_mark:**`)
-    }
-  if (message.content.startsWith(adminprefix + 'us')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**us ..**${argresult}** `)
-  } else
-  if (message.content.startsWith(adminprefix + 'av')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**avatar ... :** `);
-  }
-  });
-client.login(process.env.BOT_TOKEN);
+   client.user.setStatus("dnd")
+});
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
